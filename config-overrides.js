c133e11435288aa6e17347596e4f5e5dd06f775c
JW9506 <cf.dota@gmail.com>
2020-04-08 11:23:09 -0400

    Init Color-App

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,16 @@
-const path = require("path");
+const path = require("path")
 module.exports = {
-  webpack: function(config, env) {
+  webpack: function (config, env) {
     config.resolve.alias = Object.assign({}, config.resolve.alias, {
-      components: path.join(__dirname, "src/components")
-    });
-    return config;
+      components: path.join(__dirname, "src/components"),
+    })
+    return config
   },
-  devServer: function(configFunction) {
-    return function(proxy, allowedHost) {
-      const config = configFunction(proxy, allowedHost);
-      config.host = "127.0.0.1";
-      return config;
-    };
-  }
-};
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost)
+      config.host = "127.0.0.1"
+      return config
+    }
+  },
+}
