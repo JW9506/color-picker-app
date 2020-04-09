144fd218a84889ee3436aeeefa3af2cea49ff044
JW9506 <cf.dota@gmail.com>
2020-04-09 04:12:20 -0400

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 import Palette from "components/Palette"
 import seedColors from "seedColors"
 import generatePalette from "utils/colorHelper"
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Palette {...generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <Palette {...generatePalette(seedColors[4])} />}
+      />
+    </Switch>
   )
 }
 
