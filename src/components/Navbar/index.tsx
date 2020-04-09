8296f67f88979541d01eacab2b60950dd5245017
JW9506 <cf.dota@gmail.com>
2020-04-09 03:36:16 -0400

    Add Snackbar, style Select

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,6 +4,9 @@ import Slider from "rc-slider"
 import "rc-slider/assets/index.css"
 import Select, { SelectProps } from "@material-ui/core/Select"
 import MenuItem from "@material-ui/core/MenuItem"
+import Snackbar from "@material-ui/core/Snackbar"
+import CloseIcon from "@material-ui/icons/Close"
+import IconButton from "@material-ui/core/IconButton"
 
 interface Props {
   level: number
@@ -12,24 +15,32 @@ interface Props {
 }
 interface State {
   format: string
+  open: boolean
 }
 class Navbar extends React.Component<Props, State> {
   state: State = {
     format: "hex",
+    open: false,
   }
   handleSelectChange: SelectProps["onChange"] = (e) => {
     this.setState(
-      {
+      (s) => ({
         format: e.target.value as string,
-      },
+        open: !s.open,
+      }),
       () => {
         this.props.handleChange(this.state.format)
       }
     )
   }
+  closeSnackbar = () => {
+    this.setState((s) => ({
+      open: !s.open,
+    }))
+  }
   render() {
     const { level, changeLevel } = this.props
-    const { format } = this.state
+    const { format, open } = this.state
     return (
       <header className="Navbar">
         <div className="logo">
@@ -54,6 +65,28 @@ class Navbar extends React.Component<Props, State> {
             <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}!
+            </span>
+          }
+          ContentProps={{ "aria-describedby": "message-id" }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
       </header>
     )
   }
