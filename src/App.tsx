cf3c497bb31e0f82b2862256b9ceda253b3a2cec
JW9506 <cf.dota@gmail.com>
2020-04-08 22:37:40 -0400

    Add Color Helper to generate colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 import Palette from "components/Palette"
 import seedColors from "seedColors"
+import generatePalette from "utils/colorHelper"
 
 const App: React.FC = () => {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div>
       <Palette {...seedColors[4]} />
---
+++ src/utils/colorHelper.ts
@@ -0,0 +1,42 @@
+import chroma from "chroma-js"
+import { PaletteShape } from "seedColors"
+
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+export interface ExpandedPalette {
+  palette: string
+  id: string
+  emoji: string
+  colors: { [level: number]: { [field: string]: string }[] }
+}
+
+export default function generatePalette(starterPalette: PaletteShape) {
+  let newPalette: ExpandedPalette = {
+    palette: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
+    colors: {},
+  }
+  for (const level of levels) newPalette.colors[level] = []
+  for (const { color, name } of starterPalette.colors) {
+    const scale = generateScale(color, 10).reverse()
+    for (const i in scale)
+      newPalette.colors[levels[i]].push({
+        name: `${name} ${levels[i]}`,
+        id: name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i]).css().replace(")", ",1.0)"),
+      })
+  }
+  return newPalette
+}
+
+function getRange(hexColor: string): string[] {
+  const end = "#fff"
+  return [chroma(hexColor).darken(1.4).hex(), hexColor, end]
+}
+
+function generateScale(hexColor: string, numberOfColors: number): string[] {
+  return chroma.scale(getRange(hexColor)).mode("lab").colors(numberOfColors)
+}
