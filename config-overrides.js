acf2c6b31137cdfa088d53193aa2675602f2603d
JW9506 <cf.dota@gmail.com>
2020-04-11 08:42:44 -0400

    Add sass-resources-loader package and push to CRA SCSS loader

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,16 @@ module.exports = {
     config.resolve.alias = Object.assign({}, config.resolve.alias, {
       components: path.join(__dirname, "src/components"),
     })
+    const SASS_LOADER = config.module.rules[2].oneOf.find((rules) => {
+      if (rules.test instanceof RegExp) return rules.test.test(".scss")
+      return false
+    })
+    SASS_LOADER.use.push({
+      loader: "sass-resources-loader",
+      options: {
+        resources: ["src/global.scss"],
+      },
+    })
     return config
   },
   devServer: function (configFunction) {
