993087a530b8f9e314d2744c439baf23eaff80c4
JW9506 <cf.dota@gmail.com>
2020-04-11 09:49:37 -0400

    Updates

---
+++ precache-manifest.90e54492a80c2a41dbc47f830116a53c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fcaba14d6aabcc7baa4410f5b69139",
+    "url": "/color-picker-app/index.html"
+  },
+  {
+    "revision": "07a6daf01eeb5bc68eb0",
+    "url": "/color-picker-app/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "ff376851c6b6b7d0ac07",
+    "url": "/color-picker-app/static/css/main.1778c3d2.chunk.css"
+  },
+  {
+    "revision": "07a6daf01eeb5bc68eb0",
+    "url": "/color-picker-app/static/js/2.f59362cf.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-picker-app/static/js/2.f59362cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff376851c6b6b7d0ac07",
+    "url": "/color-picker-app/static/js/main.fc858344.chunk.js"
+  },
+  {
+    "revision": "4d5c8855858f453a589d",
+    "url": "/color-picker-app/static/js/runtime-main.a040adcd.js"
+  }
+]);
