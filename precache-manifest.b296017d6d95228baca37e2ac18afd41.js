405f66e28b9e03d98d8b68eb7da7d54f70d6ae02
JW9506 <cf.dota@gmail.com>
2020-04-11 10:36:43 -0400

    Updates

---
+++ precache-manifest.b296017d6d95228baca37e2ac18afd41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2f7bef690d31f8b52405911e7ad36b",
+    "url": "color-picker-app/index.html"
+  },
+  {
+    "revision": "07a6daf01eeb5bc68eb0",
+    "url": "color-picker-app/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "3d0acfacb99a0f5ae5ab",
+    "url": "color-picker-app/static/css/main.1778c3d2.chunk.css"
+  },
+  {
+    "revision": "07a6daf01eeb5bc68eb0",
+    "url": "color-picker-app/static/js/2.f59362cf.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "color-picker-app/static/js/2.f59362cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0acfacb99a0f5ae5ab",
+    "url": "color-picker-app/static/js/main.0e129373.chunk.js"
+  },
+  {
+    "revision": "dcca19b9f2dde0ddebb7",
+    "url": "color-picker-app/static/js/runtime-main.fc3fe6be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-picker-app/precache-manifest.82d51b0766815e3c64321198f5980971.js"
+  "color-picker-app/precache-manifest.b296017d6d95228baca37e2ac18afd41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-picker-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("color-picker-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
